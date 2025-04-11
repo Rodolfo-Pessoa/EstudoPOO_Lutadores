5d44bb7237f2bed9a0647f620cd4aa511a53f624
Rodolfo-Pessoa <rodolfolujcaspessoa@hotmail.com>
2025-04-11 14:40:38 -0300

    Frist Commit

--- src/Lutador/Lutador.java
+++ src/Lutador/Lutador.java
@@ -1,4 +1,132 @@
 package Lutador;
 
+import java.lang.reflect.Constructor;
+
 public class Lutador {
+
+    //Atributos:--------------------------------
+
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private double altura;
+    private double peso;
+    private String categoria;
+    private int vitoria;
+    private int derrota;
+    private int empate;
+
+    //Método (Especiail) Construtor:-----------------------------
+
+    public Lutador(String no, String na, int id, double al, double pe, int vi, int de, int em) {
+
+        this.nome = no;
+        this.nacionalidade = na;
+        this.idade = id;
+        this.altura = al;
+        this.peso = pe;
+        this.vitoria = vi;
+        this.derrota = de;
+        this.empate = em;
+    }
+
+    //Métodos:--------------------------------------
+
+    public void apresentar() {
+        System.out.println("Lutador: " + this.getNome());
+        System.out.println("Nacionalidade: " + this.getNacionalidade());
+        System.out.println("Idade: " + this.getIdade());
+        System.out.println("Altura: " + this.getAltura());
+        System.out.println("Peso: " + this.getPeso());
+        System.out.println("Vitórias: " + this.getVitoria());
+        System.out.println("derrotas: " + this.getDerrota());
+        System.out.println("Empates: " + this.getEmpate());
+
+    }
+    public void status() {
+
+    }
+    public void ganharLuta() {
+
+    }
+    public void perderLuta() {
+
+    }
+    public void empatarLuta() {
+
+    }
+    //Getters e Setters:-----------------------------------
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String no) {
+        this.nome = no;
+    }
+//-------------------------------------------
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String na) {
+        this.nacionalidade = na;
+    }
+//--------------------------------------------------
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int id) {
+        this.idade = id;
+    }
+//----------------------------------------------------
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double al) {
+        this.altura = al;
+    }
+//--------------------------------------------------------
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double pe) {
+        this.peso = pe;
+       // this.setCategoria();
+    }
+//-----------------------------------------------------------------
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String ca) {
+        this.categoria = ca;
+    }
+//-----------------------------------------------------------
+    public int getVitoria() {
+        return vitoria;
+    }
+
+    public void setVitoria(int vi) {
+        this.vitoria = vi;
+    }
+//-----------------------------------------------------------
+    public int getDerrota() {
+        return derrota;
+    }
+
+    public void setDerrota(int de) {
+        this.derrota = de;
+    }
+    //-----------------------------------------------------------
+    public int getEmpate() {
+        return empate;
+    }
+
+    public void setEmpate(int em) {
+        this.empate = em;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Lutador.Lutador;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+//EXISTE ESSA É UMA FORMA DE FAZER
+//Leves:
+Lutador l1 = new Lutador("Pretty Boy", "França", 31, 1.75, 68.9, 11, 2, 1 );
+Lutador l2 = new Lutador("Putscrip", "Brasil", 29, 1.68, 57.8, 14, 2, 1);
+
+//Médios:
+Lutador l3 = new Lutador("Snashadow", "EUA", 35, 1.65, 80.9, 12, 2, 1);
+Lutador l4 = new Lutador("Dead Code", "Austrália", 28, 1.94, 81.6, 13, 0, 2);
+
+//Pesados:
+Lutador l5 = new Lutador("Ufocobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
+Lutador l6 = new Lutador("Nerdaard", "EUA", 30, 1.81, 105.7, 12, 2, 4);
+
+// MAS PODEMOS FAZER EM FORMAS DE VETOR!----------------------
+
+
+//--------------------------------------------------------------
+        System.out.println("E COM VOCÊS ");
+        System.out.println("");
+        System.out.println("Para as lutas da noite temos:");
+        l1.apresentar();
+        System.out.println("___________X___________");
+        l2.apresentar();
+        System.out.println("");
+        System.out.println("----------------------------------------------");
+        l3.apresentar();
+        System.out.println("___________X___________");
+        l4.apresentar();
+        System.out.println("");
+        System.out.println("----------------------------------------------");
+        l5.apresentar();
+        System.out.println("___________X___________");
+        l6.apresentar();
+        System.out.println("");
+        System.out.println("----------------------------------------------");
     }
 }
